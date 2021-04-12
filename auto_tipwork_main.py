a3e8c6cf1d27cef28bdcfb00a89c69a19133f943
Shenkai Wang <shenkaiwang@berkeley.edu>
2021-04-12 11:12:25 -0700

    added disclaimer

--- auto_tipwork_main.py
+++ auto_tipwork_main.py
@@ -2,9 +2,27 @@
 
 Module: auto_tipwork
 
-Author: Shenkai Wang, Junmian Zhu and Raymond Blackwell
+Author: Shenkai Wang, Junmian Zhu, Raymond Blackwell, and Felix R. Fischer
 
 Description: Main Function for Autotipwork. This Function can be Run from here or the UI
+
+UC Berkeley's Copyright and Disclaimer Notice
+
+Copyright ©2021. The Regents of the University of California (Regents). All Rights Reserved.
+Permission to use, copy, modify, and distribute this software and its documentation for
+educational, research, and not-for-profit purposes, without fee and without a signed licensing agreement,
+is hereby granted, provided that the above copyright notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620,
+(510) 643-7201, otl@berkeley.edu, http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 """
 
